f99851185640790a5733b8e297f48b97f970e544
sadatrafsanjani <sadatrafsanjani@gmail.com>
2022-06-03 23:37:16 +0600

    project updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IndexComponent } from './index/index.component';
-import { NavigationComponent } from './navigation/navigation.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
 import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
-import { FooterComponent } from './footer/footer.component';
 import {HttpClientModule} from "@angular/common/http";
-import { ReferenceComponent } from './reference/reference.component';
+import {NavigationComponent} from "./template/navigation/navigation.component";
+import {SidebarComponent} from "./template/sidebar/sidebar.component";
+import {FooterComponent} from "./template/footer/footer.component";
 
 @NgModule({
   declarations: [
@@ -16,8 +15,7 @@ import { ReferenceComponent } from './reference/reference.component';
     IndexComponent,
     NavigationComponent,
     SidebarComponent,
-    FooterComponent,
-    ReferenceComponent
+    FooterComponent
   ],
   imports: [
     BrowserModule,
--- src/app/template/sidebar/sidebar.component.ts
+++ src/app/template/sidebar/sidebar.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { faGithub, faLinkedin, faYoutube } from '@fortawesome/free-brands-svg-icons';
-import { faPerson, faTools, faBuilding, faUniversity, faImages, faChartPie, faExternalLink } from '@fortawesome/free-solid-svg-icons';
+import { faPerson, faTools, faBuilding, faUniversity, faImages, faChartPie } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-sidebar',
@@ -18,7 +18,6 @@ export class SidebarComponent implements OnInit {
   faUniversity = faUniversity;
   faImages = faImages;
   faChartPie = faChartPie;
-  faExternalLink = faExternalLink;
 
 
   constructor() { }
