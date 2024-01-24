1120846d2ccc820e653f16cb2709aa1d47447503
sadatrafsanjani <sadatrafsanjani@gmail.com>
2024-01-25 00:28:22 +0600

    page not found added

--- src/app/page-not-found/page-not-found.component.ts
+++ src/app/page-not-found/page-not-found.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import {NgxSpinnerModule} from "ngx-spinner";
+import {ReactiveFormsModule} from "@angular/forms";
+import {SharedModule} from "../shared/shared.module";
 
 @Component({
   selector: 'app-page-not-found',
   standalone: true,
-  imports: [],
+    imports: [
+        NgxSpinnerModule,
+        ReactiveFormsModule,
+        SharedModule
+    ],
   templateUrl: './page-not-found.component.html',
   styleUrl: './page-not-found.component.css'
 })
